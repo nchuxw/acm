7b44ac93ef3702b4f0e1dc06e5c6fc0d60682349
nchuxw <16201325@stu.nchu.edu.cn>
2020-02-13 19:45:16 +0800

    2G TLE

--- kuangbin/02/G_HDU_3085.cpp
+++ kuangbin/02/G_HDU_3085.cpp
@@ -5,152 +5,97 @@
 
 using namespace std;
 
-const int MAX_N = 800 + 10;
-// const int MAX_N = 2 + 10;
+// const int MAX_N = 800 + 10;
+const int MAX_N = 2 + 10;
 
 typedef struct point
 {
-	int x, y, t;
+	int x, y;
 } point;
 
+int n, m;
 char mp[MAX_N][MAX_N];
+char book[2][MAX_N][MAX_N];
+queue<point> que[2];
 int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-int dir2[12][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {0, 2}, {0, -2}, {2, 0}, {-2, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-// int dir3[24][2] = 
-// {
-// 	{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {0, 2}, {0, -2}, {-2, 0}, {2, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
-// 	{0, 3}, {0, -3}, {3, 0}, {-3, 0}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}
-// };
 
-int bfs(int n, int m, point ms, point gs, point zs[])
+int abs(int a, int b)
 {
-	int t, x, y, i;
-	point tep;
-	queue<point> mq;
-	queue<point> gq;
-	queue<point> zq;
-	queue<point> que;
+	return (a > b) ? a - b : b - a;
+}
 
-	mq.push(ms);
-	gq.push(gs);
-	zq.push(zs[0]);
-	zq.push(zs[1]);
-	while(mq.empty() == false && gq.empty() == false)
+int check_point(point p, point zs[], int t)
+{
+	if (abs(p.x, zs[0].x) + abs(p.y, zs[0].y) <= 2 * t || 
+		abs(p.x, zs[1].x) + abs(p.y, zs[1].y) <= 2 * t)
 	{
-		t = 1e9;
-		if(zq.empty() == false && t > zq.front().t)
-		{
-			t = zq.front().t;
-		}
-		if(t > mq.front().t)
-		{
-			t = mq.front().t;
-		}
-		if(t > gq.front().t)
-		{
-			t = gq.front().t;
-		}
+		return 1;
+	}
+	return 0;
+}
 
-		while(zq.empty() == false && zq.front().t == t)
-		{
-			tep = zq.front();
-			zq.pop();
-			for(i = 0; i < 12; i++)
-			{
-				x = tep.x + dir2[i][0];
-				y = tep.y + dir2[i][1];
-				if(x < 0 || n <= x || y < 0 || m <= y || mp[x][y] == 'Z' || mp[x][y] == 'X')
-				{
-					continue;
-				}
-				mp[x][y] = 'Z';
-				zq.push((point){x, y, t + 1});
-			}
-		}
+int step_one(int mg, point zs[], int t)
+{
+	int i, pn;
+	point front, tep;
 
-		while(mq.empty() == false && mq.front().t == t)
+	pn = que[mg].size();
+	while(pn--)
+	{
+		front = que[mg].front();
+		que[mg].pop();
+		if(check_point(front, zs, t))
 		{
-			tep = mq.front();
-			mq.pop();
-			if(mp[tep.x][tep.y] != 'M')
+			continue;
+		}
+		for(i = 0; i < 4; i++)
+		{
+			tep = front;
+			tep.x += dir[i][0];
+			tep.y += dir[i][1];
+			if(tep.x < 0 || tep.x >= n || tep.y < 0 || tep.y >= n)
 			{
 				continue;
 			}
-			tep.t = 0;
-			que.push(tep);
-		}
-		while(que.empty() == false)
-		{
-			tep = que.front();
-			que.pop();
-			tep.t++;
-			if(tep.t > 3)
+			if(check_point(tep, zs, t) || mp[tep.x][tep.y] == 'X' || book[mg][tep.x][tep.y] == 1)
 			{
 				continue;
 			}
-			for(i = 0; i < 4; i++)
+			if(book[!mg][tep.x][tep.y] == 1)
 			{
-				x = tep.x + dir[i][0];
-				y = tep.y + dir[i][1];
-				if(mp[x][y] == 'G')
-				{
-					return t + 1;
-				}
-				if(x < 0 || n <= x || y < 0 || m <= y || mp[x][y] != '.')
-				{
-					continue;
-				}
-				mp[x][y] = 'M';
-				que.push((point){x, y, tep.t});
-				mq.push((point){x, y, t + 1});
+				return 1;
 			}
+			book[mg][tep.x][tep.y] = 1;
+			que[mg].push(tep);
 		}
+	}
+	return 0;
+}
 
-		while(gq.empty() == false && gq.front().t == t)
-		{
-			tep = gq.front();
-			gq.pop();
-			if(mp[tep.x][tep.y] != 'G')
-			{
-				continue;
-			}
-			tep.t = 0;
-			que.push(tep);
-		}
-		while(que.empty() == false)
-		{
-			tep = que.front();
-			que.pop();
-			tep.t++;
-			if(tep.t > 1)
-			{
-				continue;
-			}
-			for(i = 0; i < 4; i++)
-			{
-				x = tep.x + dir[i][0];
-				y = tep.y + dir[i][1];
-				if(mp[x][y] == 'M')
-				{
-					return t + 1;
-				}
-				if(x < 0 || n <= x || y < 0 || m <= y || mp[x][y] != '.')
-				{
-					continue;
-				}
-				mp[x][y] = 'G';
-				que.push((point){x, y, tep.t});
-				gq.push((point){x, y, t + 1});
-			}
-		}
+int solve(point ms, point gs, point zs[])
+{
+	int t;
+
+	memset(book, 0, sizeof(book));
+	book[0][ms.x][ms.y] = 1;
+	book[1][gs.x][gs.y] = 1;
+	que[0].push(ms);
+	que[1].push(gs);
+	t = 0;
+	while(que[0].empty() == false || que[1].empty() == false)
+	{
+		t++;
+		if(step_one(0, zs, t) == 1) return t;
+		if(step_one(0, zs, t) == 1) return t;
+		if(step_one(0, zs, t) == 1) return t;
+		if(step_one(1, zs, t) == 1) return t;
 	}
 	return -1;
 }
 
 int main()
 {
-	int t, n, m;
-	int i, j, k, ans;
+	int t, i, j, k, ans;
 	point ms, gs, zs[2];
 
 	scanf("%d", &t);
@@ -166,21 +111,23 @@ int main()
 			{
 				if(mp[i][j] == 'M')
 				{
-					ms = (point){i, j, 0};
+					ms = (point){i, j};
 				}
 				else if(mp[i][j] == 'G')
 				{
-					gs = (point){i, j, 0};
+					gs = (point){i, j};
 				}
 				else if(mp[i][j] == 'Z')
 				{
-					zs[k] = (point){i, j, 0};
+					zs[k] = (point){i, j};
 					k++;
 				}
 			}
 		}
 
-		ans = bfs(n, m, ms, gs, zs);
+		while(que[0].empty() == false) que[0].pop();
+		while(que[1].empty() == false) que[1].pop();		
+		ans = solve(ms, gs, zs);
 		printf("%d\n", ans);
 	}
 	return 0;
